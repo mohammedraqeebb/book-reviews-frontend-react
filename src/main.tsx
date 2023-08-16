7b09908984daed6b99fdfd4e0ebd1b640aae56ba
Mohammed Raqeeb <mohammedraqeeb9999@gmail.com>
2023-08-16 16:26:22 +0530

    fix: updated backend url

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,7 @@ import '../src/styles/globals.scss';
 
 import { PortalProvider } from './contexts/portal-context';
 
-export const BACKEND_URL =
-  'https://book-reviews-backend-latest.onrender.com/api';
+export const BACKEND_URL = 'https://book-reviews-backend-tlw2.onrender.com';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
